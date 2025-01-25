3df8f1499ec3f2aa0396209b205ac08ee3f73e09
ronesta <gvozzzd123@gmail.com>
2025-01-25 21:38:02 +0300

    Initial commit

---
+++ iTunesUserDefaultsMVVM/iTunesUserDefaultsMVVM/Album.swift
@@ -0,0 +1,20 @@
+//
+//  Album.swift
+//  iTunesUserDefaultsMVVM
+//
+//  Created by Ибрагим Габибли on 25.01.2025.
+//
+
+import Foundation
+
+struct PostAlbums: Codable {
+    let results: [Album]
+}
+
+struct Album: Codable {
+    let artistId: Int
+    let artistName: String
+    let collectionName: String
+    let artworkUrl100: String
+    let collectionPrice: Double
+}
---
+++ iTunesUserDefaultsMVVM/iTunesUserDefaultsMVVM/Network/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  iTunesUserDefaultsMVVM
+//
+//  Created by Ибрагим Габибли on 25.01.2025.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
